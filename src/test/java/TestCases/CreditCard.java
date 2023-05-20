1180a64b10fedf7820409e09df602d7891878ac4
Your Name <you@example.com>
2023-05-20 10:45:44 +0000

    command

---
+++ src/test/java/TestCases/CreditCard.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+
+public class CreditCard extends BaseCase 
+{
+@Test(groups= {"sanity"})
+	
+	public void test4() {
+		System.out.println("Inside home loan test 4");
+	}
+
+}
